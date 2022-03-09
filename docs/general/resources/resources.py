595330e2cd0e0006a77335853abdc264e236a63f
Alian713 <divy1211.dc@gmail.com>
2022-03-09 23:19:10 +0800

    synced resource names

--- docs/general/resources/resources.py
+++ docs/general/resources/resources.py
@@ -10,14 +10,14 @@ This page is a list of all the player resources in the scenario editor and their
 with open("./res_desc.json") as file:
     data = json.load(file)
     for resource in data:
-        md+=f"## {int(resource)+1}. {data[resource]['name']}\n\n- ID: {resource}\n\n" + \
-            f"- Purpose: {data[resource]['keyword']} {data[resource]['grammar']}\n\n"
-        if data[resource]['default_values']:
+        md+=f"## {resource}. {data[resource]['name']}\n\n- ID: {resource}\n\n" + \
+            f"- Purpose: {data[resource]['desc']}\n\n"
+        if data[resource]['defaults']:
             md+="- Default Values:\n\n"
-        for value in data[resource]['default_values']:
-            md+=f"\t- {value}: {data[resource]['default_values'][value]}\n\n"
-        if data[resource]['comment']:
-            md+= f"- Additional Information: {data[resource]['comment']}\n\n"
+        for value in data[resource]['defaults']:
+            md+=f"\t- {value}: {data[resource]['defaults'][value]}\n\n"
+        if data[resource]['note']:
+            md+= f"- Note: {data[resource]['note']}\n\n"
 
 links = """ [here](https://youtu.be/-qRUaOHpbwI?t=870 "Explanatory video by T-West")
  [here](https://youtu.be/-qRUaOHpbwI?t=830 "Explanatory video by T-West")
